8f5574d90158fa7f38eadece808387e5b2a80eda
gujima <2010902h@stu.kobe-u.ac.jp>
2021-10-19 18:11:27 +0900

    change 3-q1

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -1,16 +1,43 @@
 // 練習問題「心臓の鼓動のように大きくなったり小さくなったりする円」
-let count;
-let cycle;
+//let count;
+//let cycle;
 
-function setup(){
-  createCanvas(200, 200);
-  count = 0;
-  cycle = 100;
-}
+//function setup(){
 
-function draw(){
-  background(160, 192, 255);
-  count = (count + 1) % cycle;
+  // count = 0;
+  // cycle = 100;
+// }
+
+// function draw(){
+  // background(160,192,255);
+  // count = (count + 1) % cycle;
   // BLANK[1]
-  ellipse(width / 2, height / 2, size);
+let x,y,z,vz;
+
+  function setup(){
+    createCanvas(windowWidth, windowHeight);
+    x = width / 2;
+    y = height / 2;
+    z = 50;
+    vz = 1;
+  }
+
+  function windowResized(){
+    resizeCanvas(windowWidth, windowHeight);
+  }
+
+
+  function draw(){
+    background(160, 192, 255);
+
+ellipse(x, y, z);
+ z += vz;
+ if(z < 50  || z > 100){ vz = -1 * vz; }
 }
+
+function keyPressed(){
+  if(key == " "){ vz = 2* vz; }
+}
+  function keyReleased(){
+    if(key == " "){ vz = 0.5*vz;}
+  }
