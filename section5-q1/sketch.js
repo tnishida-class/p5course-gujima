56be70fbd6dc01f9291f6b293eea3aab766482de
gujima <2010902h@stu.kobe-u.ac.jp>
2021-11-11 21:57:29 +0900

    change 5-q1

--- section5-q1/sketch.js
+++ section5-q1/sketch.js
@@ -4,15 +4,23 @@
 function setup(){
   createCanvas(400, 400);
   background(255);
-  balloon("I love keyakizaka46");
+  balloon("I love keyakizaka46",50);
 }
 
-function balloon(t){
+function balloon(t,n){
   let w = textWidth(t);
   let h = textAscent() + textDescent();
-  let p = 2;
-  fill(0);
-  rect(0, 0, w + p * 2, h + p * 2);
+  let p = n;
+  fill(0,100,120);
+  noStroke();
+  ellipse((w+p*2) / 2 , h/1.5 + p , w*1.5 , h * 1.5 );
   fill(255);
   text(t, p, h + p);
+  fill(0,100,120);
+  noStroke();
+  beginShape();
+  vertex(w*7/6 + p , h/1.5 + p);
+  vertex((w*7/6 + p)*7/8, (h/1.5 + p) + h/3);
+  vertex(w*7/6 + p, h/1.5 + p + h+1.5);
+  endShape();
 }
